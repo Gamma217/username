f9d2b249d5cf9451afef844ea18425348ced2189
Ethan Clunie <ethancgames@yahoo.com>
2020-11-15 18:33:01 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -7,7 +7,7 @@ echo "The username must begin with a lowercase character"
 echo "The username must contain at least 3 but no more than 12 characters"
 
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username!"
 	echo "Enter a valid username: "
